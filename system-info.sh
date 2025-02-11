3f30a0716c6669a5e4f74be7960cd8dbac7e05b3
root <root@mail.gaihre94704.mytld>
2025-02-11 06:03:52 +0000

    Initial commit - System Information Script

---
+++ system-info.sh
@@ -0,0 +1,27 @@
+echo "--------------------"
+echo "CPU: $CPU"
+echo "RAM: $RAM"
+echo "Disks: $DISKS"
+echo "Video: $VIDEO"
+echo ""
+echo "Network Information"
+echo "-------------------"
+echo "FQDN: $FQDN"
+echo "Host Address: $IP_ADDRESS"
+echo "Gateway IP: $GATEWAY"
+echo "DNS Server: $DNS_SERVER"
+echo ""
+echo "System Status"
+echo "-------------"
+echo "Users Logged In: $USERS"
+echo "Disk Space: $DISK_SPACE"
+echo "Process Count: $PROCESS_COUNT"
+echo "Load Averages: $LOAD_AVERAGES"
+echo "Listening Network Ports: $LISTENING_PORTS"
+echo "UFW Status: $UFW_STATUS"
+echo ""
+
+
+
+
+
