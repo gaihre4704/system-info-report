13056d124a57dd96f1410be11d40edc50d6edb50
Bishal Gaihre <bishalgaihre76@gmail.com>
2025-02-11 06:23:30 +0000

    Initial commit - System Information Script

--- system-info.sh
+++ system-info.sh
@@ -1,3 +1,31 @@
+#!/bin/bash
+
+# Gather system information
+USERNAME=$(whoami)
+DATE_TIME=$(date)
+HOSTNAME=$(hostname)
+OS=$(source /etc/os-release && echo "$PRETTY_NAME")
+UPTIME=$(uptime -p)
+
+# Gather hardware information
+CPU=$(lscpu | grep "Model name" | cut -d ':' -f2 | xargs)
+RAM=$(free -h | awk '/^Mem:/ {print $2}')
+DISK_INFO=$(lsblk -o MODEL,SIZE | grep -v "MODEL")
+VIDEO_CARD=$(lshw -C display 2>/dev/null | grep "product" | cut -d ':' -f2 | xargs)
+
+# Gather network information
+FQDN=$(hostname --fqdn)
+HOST_IP=$(ip route get 1 | awk '{print $7; exit}')
+GATEWAY_IP=$(ip route | grep default | awk '{print $3}')
+DNS_SERVER=$(grep "nameserver" /etc/resolv.conf | awk '{print $2}' | head -n 1)
+
+# Gather system status information
+USERS_LOGGED_IN=$(who | awk '{print $1}' | sort | uniq | paste -sd ", ")
+DISK_SPACE=$(df -h --output=source,avail | grep '^/' | awk '{print $1 ": " $2}' | paste -sd ", ")
+PROCESS_COUNT=$(ps aux | wc -l)
+LOAD_AVERAGES=$(uptime | awk -F 'load average:' '{print $2}' | xargs)
+LISTENING_PORTS=$(ss -tuln | awk '{print $5}' | grep -Eo '[0-9]+$' | sort -n | uniq | paste -sd ", ")
+UFW_STATUS=$(sudo ufw status | grep "Status" | awk '{print $2}')
 echo "--------------------"
 echo "CPU: $CPU"
 echo "RAM: $RAM"
